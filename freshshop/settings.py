6f093b8945bd5943011ffeecc22346052473157b
huatouDD <huatoudd@163.com>
2020-01-16 20:14:42 +0800

    update xadmin bug

--- freshshop/settings.py
+++ freshshop/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'user_operation',
     'goods',
     'rest_framework',
-    'xadmin'
+    'xadmin',
+    'crispy_forms'
 
 ]
 
@@ -119,15 +120,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
