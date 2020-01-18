f549a84ba1bf4e0f2e74690692217543ee1b0c56
huatouDD <huatoudd@163.com>
2020-01-18 10:14:37 +0800

    goods model

--- apps/goods/models.py
+++ apps/goods/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.timezone import now
 
-
 # Create your models here.
 from DjangoUeditor.models import UEditorField
 
@@ -40,7 +39,7 @@ class Goods(models.Model):
     商品
     """
     category = models.ForeignKey(GoodsCategory, on_delete=models.CASCADE, verbose_name="商品类目")
-    goods_sn = models.CharField("商品唯一货号",max_length=50, default="")
+    goods_sn = models.CharField("商品唯一货号", max_length=50, default="")
     name = models.CharField("商品名", max_length=100, )
     click_num = models.IntegerField("点击数", default=0)
     sold_num = models.IntegerField("商品销售量", default=0)
@@ -68,6 +67,26 @@ class Goods(models.Model):
         return self.name
 
 
+class GoodsCategoryBrand(models.Model):
+    """
+    某一大类下的宣传商标
+    """
+    category = models.ForeignKey(GoodsCategory, on_delete=models.CASCADE, related_name='brands', null=True, blank=True,
+                                 verbose_name="商品类目")
+    name = models.CharField("品牌名", default="", max_length=30, help_text="品牌名")
+    desc = models.TextField("品牌描述", default="", max_length=200, help_text="品牌描述")
+    image = models.ImageField(max_length=200, upload_to="brands/")
+    add_time = models.DateTimeField("添加时间", default=now)
+
+    class Meta:
+        verbose_name = "宣传品牌"
+        verbose_name_plural = verbose_name
+        db_table = "goods_goodsbrand"
+
+    def __str__(self):
+        return self.name
+
+
 class GoodsImage(models.Model):
     """
     商品轮播图
@@ -84,59 +103,50 @@ class GoodsImage(models.Model):
         return self.goods.name
 
 
+# 因为首页的轮播图跟商品平常的图片一样，所以需要区分来
+class Banner(models.Model):
+    """
+    首页轮播的商品
+    """
+    goods = models.ForeignKey(Goods, on_delete=models.CASCADE, verbose_name="商品")
+    image = models.ImageField(upload_to='banner', verbose_name="轮播图片")
+    index = models.IntegerField("轮播顺序", default=0)
+    add_time = models.DateTimeField("添加时间", default=now)
+
+    class Meta:
+        verbose_name = '首页轮播'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.goods.name
 
 
+class IndexAd(models.Model):
+    """
+    商品广告
+    """
+    category = models.ForeignKey(GoodsCategory, on_delete=models.CASCADE, related_name='category', verbose_name="商品类目")
+    goods = models.ForeignKey(Goods, on_delete=models.CASCADE, related_name='goods')
+
+    class Meta:
+        verbose_name = '首页广告'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.goods.name
 
 
+class HotSearchWords(models.Model):
+    """
+    搜索栏下方热搜词
+    """
+    keywords = models.CharField("热搜词", default="", max_length=20)
+    index = models.IntegerField("排序", default=0)
+    add_time = models.DateTimeField("添加时间", default=now)
 
+    class Meta:
+        verbose_name = '热搜排行'
+        verbose_name_plural = verbose_name
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def __str__(self):
+        return self.keywords
