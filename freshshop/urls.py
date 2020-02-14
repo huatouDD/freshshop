ab129de90739153b2259aaef230abfb183e9e2b4
huatouDD <huatoudd@163.com>
2020-02-14 09:51:04 +0800

    media配置

--- freshshop/urls.py
+++ freshshop/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
 
 import xadmin
+from freshshop.settings import MEDIA_ROOT
 
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
-    path('ueditor/', include('DjangoUeditor.urls'))
+    path('ueditor/', include('DjangoUeditor.urls')),
+    path('media/<path:path>', serve, {'document_root': MEDIA_ROOT})
 ]
