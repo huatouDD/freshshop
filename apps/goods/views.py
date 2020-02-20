25e4f8edbcfb5de2e91b068deb7088ad00b03c23
huatouDD <huatoudd@163.com>
2020-02-20 14:49:53 +0800

    区分rest_framework的继承视图

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -6,13 +6,18 @@ from rest_framework.views import APIView
 
 from goods.models import Goods
 from goods.serializers import GoodsSerializer
+from rest_framework import generics
 
 
-class GoodsListView(APIView):
-    """
-    商品列表
-    """
-    def get(self, request):
-        goods = Goods.objects.all()
-        goods_serializer = GoodsSerializer(goods, many=True)
-        return Response(goods_serializer.data)
+# class GoodsListView(APIView):
+#     """
+#     商品列表
+#     """
+#     def get(self, request):
+#         goods = Goods.objects.all()
+#         goods_serializer = GoodsSerializer(goods, many=True)
+#         return Response(goods_serializer.data)
+
+class GoodsListView(generics.ListAPIView):
+    serializer_class = GoodsSerializer
+    queryset = Goods.objects.all()
